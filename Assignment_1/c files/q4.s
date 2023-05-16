92dd8d3f3f0baa35ea60ca338ecb7b7a06d6ae37
Prithvi Karthik <79378970+Prithvi-k@users.noreply.github.com>
2023-05-16 19:47:25 +0530

    Completed bonus question

--- Assignment_1/c files/q4.s
+++ Assignment_1/c files/q4.s
@@ -5,34 +5,60 @@
 get_product_assembly:
         # %rdi = head
 
-        movq $1, %rax           # product = 1
+        movq $1, %rax                   # product = 1
 
-        testq %rdi, %rdi        # Check if head is NULL
-        je .L2                  # If so, return 1
+        testq %rdi, %rdi                # Check if head is NULL
+        je .L2                          # If so, return 1
+
+        movq $9223372036854775807, %r12 # Store LLONG_MAX in %r12
 
     #
     # Main function to calculate product
     #
     .L1:
-        testq %rdi, %rdi        # Check if head is NULL
-        je .L3                  # If so, go to .L3
+        testq %rdi, %rdi                # Check if head is NULL
+        je .L3                          # If so, go to .L3
 
-        imulq (%rdi), %rax      # product *= head->data
+        movq $0, %rdx                   # Clear %rdx
+        imulq (%rdi), %rax              # product *= head->data
+
+        cmpq $0, %rax                   # Check if product is 0
+        je .L3                          # If so, go to .L3 (skips modulo)
+
+        movq %rax, %rbx                 # Move product into %rbx
+        movq $0, %rax                   # Clear %rax
+
+        # Product modulo LLONG_MAX
+        idivq %r12                      # product /= LLONG_MAX
+        cmpq $0, %rdx                   # Check if remainder is 0
+        je .L4                          # If not, go to .L4
+
+        movq %rbx, %rax                 # Move product into %rax
 
     #
     # Traverse to next node, check if it's NULL
     #
     .L2:
-        testq %rdi, %rdi        # Check if head is NULL
-        je .L3                  # If so, go to .L3
+        testq %rdi, %rdi                # Check if head is NULL
+        je .L3                          # If so, go to .L3
 
-        addq $8, %rdi           # head = head->next
-        movq (%rdi), %rsi       # move head->next into %rsi
-        testq %rsi, %rsi        # Check if head->next is NULL
-        je .L3                  # If so, go to .L3
+        addq $8, %rdi                   # head = head->next
+        movq (%rdi), %rsi               # move head->next into %rsi
+        testq %rsi, %rsi                # Check if head->next is NULL
+        je .L3                          # If so, go to .L3
 
-        movq %rsi, %rdi         # Move back to %rdi
-        jmp .L1                 # Go to .L1
+        movq %rsi, %rdi                 # Move back to %rdi
+        jmp .L1                         # Go to .L1
+
+    #
+    # Modulo function
+    #
+    .L4:
+        movq %rbx, %rax                 # Move product into %rax
+        idivq %r12                      # product /= LLONG_MAX
+        movq %rdx, %rax                 # Move remainder into %rax
+        jmp .L2                         # Go to .L2
+
 
     #
     # Return product
